25f7f57ed784eaf9661ebf4899c124aa34c05f3a
tomertoor <tomertom150@gmail.com>
2023-01-24 00:09:34 +0200

    Added the distance calculator

--- logic.py
+++ logic.py
@@ -2,6 +2,8 @@
 SHIMSHON_ENGINE_FORCE = 1000000 # in nuetons
 SHIMSHON_TAKEOFF_VELOCITY = 140 # in m per second
 
+MAX_TAKEOFF_TIME = 60
+
 def calculate_acceleration(force, mass):
     """Calculates acceleration by force and mass
 
@@ -14,7 +16,7 @@ def calculate_acceleration(force, mass):
     """
     return force / mass
     
-def calculate_velocity(acceleration, time)
+def calculate_velocity(acceleration, time):
     """Calculates velocity by acceleration and time
     Args:
         acceleration (int): the acceleration of the plane
@@ -25,7 +27,7 @@ def calculate_velocity(acceleration, time)
     """
     return acceleration * time
 
-def calculate_takeoff_time(acceleration, velocity)
+def calculate_takeoff_time(acceleration, velocity):
     """Calculates the time in which the plane will reach a velocity in a given acceleration
     Args:
         acceleration (int): the acceleration of the plane
@@ -37,14 +39,30 @@ def calculate_takeoff_time(acceleration, velocity)
     return velocity / acceleration
 
 
-def calculate_takeoff_time(acceleration, time, start_pos=0, start_velocity=0)
-    
-    
-    pass
+def calculate_takeoff_distance(acceleration, time, start_pos=0, start_velocity=0):
+    """Calculates the takeoff distance of a given plane, based on acceleration, time, velocity at the beginning of takeoff and the starting position
+    Args:
+        acceleration (int): the acceleration of the plane
+        time (int): the it will take for the plane to takeoff
+    """
+    return (0.5 * acceleration *  pow(time, 2)) + (start_velocity * time) + start_pos
 
-def calculate_takeoff_stats(mass)
+def calculate_takeoff_stats(mass):
     acceleration = calculate_acceleration(SHIMSHON_ENGINE_FORCE, mass)
     time = calculate_takeoff_time(acceleration, SHIMSHON_TAKEOFF_VELOCITY)
-    calculate_takeoff_time(acceleration, time)
+    
+    overweight_mass = 0
+    
+
+
+    while time > MAX_TAKEOFF_TIME:
+        mass -= 1
+        overweight_mass += 1
+        acceleration = calculate_acceleration(SHIMSHON_ENGINE_FORCE, mass)
+        time = calculate_takeoff_time(acceleration, SHIMSHON_TAKEOFF_VELOCITY)
+
+
+    takeoff_distance = calculate_takeoff_distance(acceleration, time)
+    
 
 
